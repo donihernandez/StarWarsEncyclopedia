8763c84c44d5a827a7a308d29ff8b8c828fff838
Adonai Domingez <donihernandez003@gmail.com>
2023-01-16 21:33:19 -0500

    Initial Commit

---
+++ StarWarsEncyclopedia/StarWarsEncyclopediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsEncyclopediaApp.swift
+//  StarWarsEncyclopedia
+//
+//  Created by Doni on 1/16/23.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsEncyclopediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
